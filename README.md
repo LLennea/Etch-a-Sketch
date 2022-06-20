780dec41d1b70e0490db38ae59b299e3a3045365
Liam Lennea <liamlennea2002@gmail.com>
2022-06-20 13:43:16 -0600

    Add ability to change grid size

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Etch-a-Sketch
 A site with an interactable grid of squares that the user can use to draw on the screen.
+Made with HTML, CSS, and JS.
--- js/script.js
+++ js/script.js
@@ -1,16 +1,40 @@
 let grid = document.getElementById('grid');
+const gridSize = 600;
+grid.style.width = `${gridSize}px`;
+grid.style.height = `${gridSize}px`;
 
 for (let i=0; i<16; i++) {
     for (let i=0; i<16; i++) {
-        let square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        grid.appendChild(square);
+        createSquare(16);
     }
 }
-
 let squares = document.querySelectorAll('.square');
 squares.forEach((square => {
     square.addEventListener('mouseover', () => {
-        square.setAttribute('style', 'background-color: red');
+        square.style.backgroundColor = 'red';
     })
 }))
+
+function changeVal(newSize) {
+    document.getElementById('value').innerText = `Size of grid: ${newSize}x${newSize}`;
+    grid.innerHTML = ''
+    for (let i=0; i<newSize; i++) {
+        for (let i=0; i<newSize; i++) {
+            createSquare(newSize);
+        }
+    }
+    squares = document.querySelectorAll('.square');
+    squares.forEach((square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'red';
+        })
+    }))
+}
+
+function createSquare(size) {
+    let square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    square.style.width = `${gridSize / size}px`;
+    square.style.height = `${gridSize / size}px`;
+    grid.appendChild(square)
+}
